2e968dff13644f3adb1d5eb5f48436eee0dd5ad3
Matías <mullermatiash@gmail.com>
2023-05-13 01:37:51 -0300

    Landing alpha

--- funcionalidadIndex.js
+++ funcionalidadIndex.js
@@ -1,61 +1,22 @@
 
 const button = document.getElementById("darkMode");
+const body = document.body;
+const titulo = document.getElementById("titulo");
+const topic = document.getElementById("topicArt")
+const details = document.getElementById("detailsArt")
+
 var oscuro = true;
 
-button.addEventListener("click", function () {
-
-     if (!oscuro) {
-          document.body.style.background = "#152221";
-          document.body.style.color = '#ffffff';
-          button.innerHTML = 'Light mode'
+button.addEventListener("click", function() {
+     body.classList.toggle("bg-light");
+     titulo.classList.toggle("bg-gradient-light");
+     topic.classList.toggle("bg-gradient-light");
+     details.classList.toggle("bg-gradient-light");
+     if (oscuro){
+          button.innerHTML = "Dark"
      } else {
-          document.body.style.background = '#ffffff';
-          document.body.style.color = "black";
-          button.innerHTML = 'Dark mode'
-     }
-
-     oscuro = !oscuro
-});
-
-jQuery(function ($) {
-
-     // Function which adds the 'animated' class to any '.animatable' in view
-     var doAnimations = function () {
-
-          // Calc current offset and get all animatables
-          var offset = $(window).scrollTop() + $(window).height(),
-               $animatables = $('.animatable');
-
-          // Check all animatables and animate them if necessary
-          $animatables.each(function (i) {
-               var $animatable = $(this);
-
-               // Items that are "above the fold"
-               if (($animatable.offset().top + $animatable.height() + 50) < offset) {
-
-                    // Item previously wasn't marked as "above the fold": mark it now
-                    if (!$animatable.hasClass('animate-in')) {
-                         $animatable.removeClass('animate-out').css('top', $animatable.css('top')).addClass('animate-in');
-                    }
-
-               }
-
-               // Items that are "below the fold"
-               else if (($animatable.offset().top + $animatable.height() + 50) > offset) {
-
-                    // Item previously wasn't marked as "below the fold": mark it now
-                    if ($animatable.hasClass('animate-in')) {
-                         $animatable.removeClass('animate-in').css('top', $animatable.css('top')).addClass('animate-out');
-                    }
-
-               }
-
-          });
-
+          button.innerHTML = "Light"
      };
-
-     // Hook doAnimations on scroll, and trigger a scroll
-     $(window).on('scroll', doAnimations);
-     $(window).trigger('scroll');
-
-});
+     oscuro = !oscuro;
+}
+);
